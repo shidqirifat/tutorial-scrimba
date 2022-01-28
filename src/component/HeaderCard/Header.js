21282978f85bd5ff85f82962d6ad927a65dd2601
shidqirifat <rifatpangestu87@gmail.com>
2022-01-28 15:49:13 +0700

    successfully develop meme-generator using assets from API.

--- src/component/HeaderCard/Header.js
+++ src/component/HeaderCard/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './style.css';
-import logoNav from '../../logo/airbnb.svg';
 
 export default function Header() {
   return (
-    <header className='nav-container'>
-      <nav>
-        <img src={logoNav} alt='Air Bnb Logo' className='logo-nav' />
-      </nav>
+    <header className="header">
+      <img
+        src="./troll-face.png"
+        className="header--image"
+        alt="troll-face"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
-  );
+  )
 }
--- src/component/MainContentCard/MainContent.js
+++ src/component/MainContentCard/MainContent.js
@@ -1,13 +1,73 @@
 import React from 'react';
-import './style.css';
-import imageJumbotron from '../../images/jumbotron.svg';
+// import memesData from "../../DATA";
 
 export default function MainContent() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+  const [allMemeImages, setAllMemeImages] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(response => response.json())
+      .then(({ data }) => setAllMemeImages(data.memes));
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value,
+    }))
+  }
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+    const url = allMemeImages[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
+  }
+
   return (
     <main>
-      <img src={imageJumbotron} alt='Jumbotron' className='jumbotron-img' />
-      <h1>Online Experiences</h1>
-      <h3>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h3>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          onChange={handleChange}
+          value={meme.topText}
+          name="topText"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          onChange={handleChange}
+          value={meme.bottomText}
+          name="bottomText"
+        />
+        <button
+          className="form--button"
+          onClick={getMemeImage}
+        >
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img
+          src={meme.randomImage}
+          className="meme--image"
+          alt="meme"
+        />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   )
 }
